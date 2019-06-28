d56529b7cdead8660e3af6d3adcb0f8aeb777330
Sangsaka Wira <sangsakawira@gmail.com>
2019-06-28 19:53:38 +0700

    lumayan

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,13 +1,39 @@
 import React,{Component} from 'react'
 
 export default class SearchBar extends Component{
+    
+    constructor(props){
+        super(props)
+        this.state = {
+            search_key:""
+        }
+
+        this.onChangeHandle = this.onChangeHandle.bind(this)
+        this.onFormSubmit = this.onFormSubmit.bind(this)
+    }
+
+    onChangeHandle = event=>{
+        console.log(event.target.value)
+        this.setState({search_key:event.target.value})
+    }
+
+    onFormSubmit = event =>{
+        event.preventDefault()
+        console.log("Submited")
+        console.log("Search: "+this.search_key)
+        this.props.onSubmit(this.search_key)
+    }
+    
     render(){
         return(
             <div className="ui segment" style={{margin:"30px"}}>
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" placeholder="Search..."></input>
+                        <input type="text" 
+                        placeholder="Search..." 
+                        value={this.state.search_key} 
+                        onChange={this.onChangeHandle}></input>
                     </div>
                 </form>
             </div>
--- src/index.js
+++ src/index.js
@@ -9,12 +9,18 @@ class App extends Component{
         this.state = {
             searched:"Keanu Revees"
         }
+
+        this.onSearchSubmit = this.onSearchSubmit.bind(this)
+    }
+
+    onSearchSubmit(key){
+        console.log(key)
     }
 
     render(){
         return(
             <div>
-                <SearchBar></SearchBar>
+                <SearchBar onSubmit={this.onSearchSubmit}></SearchBar>
                 <h1 style={{margin:"30px"}}>Result On: {this.state.searched}</h1>
             </div>
         )
